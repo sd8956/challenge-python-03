446659d90883de1e3344c3ab73c8bf18ae0845b9
sd8956 <duquemoreno12@gmail.com>
2020-06-23 16:46:36 -0500

    Reto finalizado

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-# Resolve the problem!!
+import re, os
 
 
 def run():
-    # Start coding here
+    with open('encoded.txt','r', encoding='utf-8') as file:
+        regex = re.compile('[a-z]')
+        message = re.findall(regex, file.read())
+        print(''.join(message))
 
 
 if __name__ == '__main__':
